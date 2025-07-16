833c05f812633b52cf2a073e921cc679791a9a08
Logan Michaels <95458200+ldmichae@users.noreply.github.com>
2025-07-16 14:20:58 -0400

    Update stack.rs

--- src/gps/stack.rs
+++ src/gps/stack.rs
@@ -13,6 +13,8 @@ pub struct GeoStack {
     pub total_distance: f64,
     pub current_speed_mph: f64,
     pub current_hdop: f32,
+    pub min_time_interval_ms: u32,
+    pub min_distance_threshold: u32,
 }
 
 impl GeoStack {
@@ -23,39 +25,43 @@ impl GeoStack {
             total_distance: 0.0,
             current_speed_mph: 0.0,
             current_hdop: 0.0,
+            min_time_interval_ms: 1000,
+            min_distance_threshold: 1
         }
     }
 
     pub fn add_coords(&mut self, coords: GpsReaderResults) {
         // Make sure the new coordinates have valid lat/lon
-        if let (Some(new_lat), Some(new_lon), Some(hdop)) = (coords.lat, coords.lon, coords.hdop) {
+        if let (Some(new_lat), Some(new_lon), Some(hdop), Some(new_timestamp)) = (coords.lat, coords.lon, coords.hdop, coords.timestamp) {
+            self.current_hdop = hdop;
             if let Some(last_coord) = self.stack.last() {
-                if let (Some(prev_lat), Some(prev_lon)) = (last_coord.lat, last_coord.lon) {
-                    let distance_segment_ft = haversine_distance_ft(
-                        LatLon {
-                            latitude: prev_lat,
-                            longitude: prev_lon,
-                        },
-                        LatLon {
-                            latitude: new_lat,
-                            longitude: new_lon,
-                        },
-                    );
+                if let (Some(prev_lat), Some(prev_lon), Some(prev_timestamp)) = (last_coord.lat, last_coord.lon, last_coord.timestamp) {
+                    let time_delta = new_timestamp - prev_timestamp;
+                    if time_delta < self.min_time_interval_ms {
+                        return; // Skip this reading
+                    }
 
-                    if hdop <= 2.0 || distance_segment_ft > 6.0 {
-                        self.last_segment_distance = distance_segment_ft;
-                        self.total_distance += distance_segment_ft;
-                        self.current_speed_mph = calculate_speed(distance_segment_ft);
+                    if hdop < 2.0 {
+                        let distance_segment_ft = haversine_distance_ft(
+                            LatLon {
+                                latitude: prev_lat,
+                                longitude: prev_lon,
+                            },
+                            LatLon {
+                                latitude: new_lat,
+                                longitude: new_lon,
+                            },
+                        );
+
+                        if distance_segment_ft > self.min_distance_threshold {
+                            let _ = self.stack.push(coords);
+                            self.last_segment_distance = distance_segment_ft;
+                            self.total_distance += distance_segment_ft;
+                            self.current_speed_mph = calculate_speed(distance_segment_ft, time_delta);
+                        }
                     }
                 }
             }
-
-            // Only push if coordinates are valid
-            let _ = self.stack.push(coords);
-        }
-
-        if let Some(hdop) = coords.hdop {
-            self.current_hdop = hdop
         }
     }
 }
