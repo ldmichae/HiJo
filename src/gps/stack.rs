5375d054dbc9ff7cc98f3cccb8c0da3da87ccf5a
Logan Michaels <ldmichae@protonmail.com>
2025-07-16 22:03:31 -0400

    double ended queue

--- src/gps/stack.rs
+++ src/gps/stack.rs
@@ -1,5 +1,5 @@
 use chrono::Duration;
-use heapless::Vec;
+use heapless::{Deque, Vec};
 
 use crate::gps::{
     fns::{LatLon, calculate_speed, haversine_distance_ft},
@@ -9,7 +9,7 @@ use crate::gps::{
 const MAX_ITEMS: usize = 16;
 
 pub struct GeoStack {
-    pub stack: Vec<GpsReaderResults, MAX_ITEMS>,
+    pub stack: Deque<GpsReaderResults, MAX_ITEMS>,
     pub last_segment_distance: f64,
     pub total_distance: f64,
     pub current_speed_mph: f64,
@@ -21,7 +21,7 @@ pub struct GeoStack {
 impl GeoStack {
     pub fn new() -> Self {
         GeoStack {
-            stack: Vec::new(),
+            stack: Deque::new(),
             last_segment_distance: 0.0,
             total_distance: 0.0,
             current_speed_mph: 0.0,
@@ -31,13 +31,22 @@ impl GeoStack {
         }
     }
 
+    pub fn ring_buffer_push(&mut self, item: GpsReaderResults) {
+        if !self.stack.is_full() {
+            let _ = self.stack.push_back(item);
+        } else {
+            Deque::pop_front(&mut self.stack);
+            let _ = self.stack.push_back(item);
+        }
+    }
+
     pub fn add_coords(&mut self, coords: GpsReaderResults) {
         // Make sure the new coordinates have valid lat/lon
         if let (Some(new_lat), Some(new_lon), Some(hdop), Some(new_timestamp)) =
             (coords.lat, coords.lon, coords.hdop, coords.timestamp)
         {
             self.current_hdop = hdop;
-            if let Some(last_coord) = self.stack.last() {
+            if let Some(last_coord) = self.stack.back() {
                 if let (Some(prev_lat), Some(prev_lon), Some(prev_timestamp)) =
                     (last_coord.lat, last_coord.lon, last_coord.timestamp)
                 {
@@ -59,7 +68,7 @@ impl GeoStack {
                         );
 
                         if distance_segment_ft > self.min_distance_threshold {
-                            let _ = self.stack.push(coords);
+                            let _ = self.ring_buffer_push(coords);
                             self.last_segment_distance = distance_segment_ft;
                             self.total_distance += distance_segment_ft;
                             self.current_speed_mph =
@@ -68,7 +77,7 @@ impl GeoStack {
                     }
                 }
             }
-            let _ = self.stack.push(coords);
+            let _ = self.ring_buffer_push(coords);
         }
     }
 }
